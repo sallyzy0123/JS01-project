3cd0e967b81ca02805ec8693cb9f55930c718476
yingzh <sally_zy@protonmail.com>
2023-01-06 19:14:40 +0200

    enable user to add new todo item

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (request, response) => {
     return response.send('Hello, world!')
 })
@@ -36,7 +39,7 @@ app.put('/todos/:id/complete', (request, response) => {
         return -1;
     }
 
-    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+    fs.readFile('./store/todos.json', 'utf-8', (err,data) => {
         if (err) {
             return response.status(500).send('Sorry, something went wrong.')
         }
@@ -52,10 +55,34 @@ app.put('/todos/:id/complete', (request, response) => {
 
         fs.writeFile('./store/todos.json', JSON.stringify(todos), () => {
             return response.json({'status': 'ok'});
-        })
+        });
     })
 })
 
+app.post('/todo', (request, response) => {
+    if (!request.body.name) {
+        return response.status(400).send('Missing name');
+    }
+
+    fs.readFile('./store/todos.json', 'utf-8', (err,data) => {
+        if (err) {
+            return response.status(500).send('Sorry, something went wrong.')
+        };
+
+        const todos = JSON.parse(data);
+        const maxId = Math.max.apply(Math, todos.map(t => { return t.id}));
+
+        todos.push({
+            id: maxId + 1,
+            complete: false,
+            name: request.body.name
+        });
+
+        fs.writeFile('./store/todos.json', JSON.stringify(todos), () => {
+            return response.json({'status': 'ok'});
+        });
+    })
+});
 
 app.listen(3000,  () => {
     console.log('Application running on http://localhost:3000')
